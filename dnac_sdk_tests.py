8dffcaad762ee0c0c630b29f1000eb5e7ef7aaf7
Matt <mattyboy.watkins@gmail.com>
2020-11-30 21:14:16 +0000

    updates to dnac_sdk_tests.py

--- dnac_sdk_tests.py
+++ dnac_sdk_tests.py
@@ -9,13 +9,16 @@ USER = variables.DNAC_USER
 PASSWORD = variables.DNAC_PASSWORD
 VERSION = variables.DNAC_VERSION
 
-dnac = api.DNACenterAPI(username=USER, password=PASSWORD, base_url=SERVER, version=VERSION)
+dnac = api.DNACenterAPI(username=USER, password=PASSWORD, base_url=SERVER, version=VERSION, verify=False)
 
-devices = dnac.devices.get_device_list(reachability_status='Reachable', family='Unified AP')['response']
+devices = dnac.devices.get_device_list(
+   reachability_status='Reachable',
+   family='Unified AP'
+)
 
 count = 0
-for device in devices:
-   print(f"{device['id']}   {device['hostname']}")
-   count += 1
+for device in devices.response:
+    print(device.id + "  " + device.hostname)
+    count += 1
 
 print(f'Count: {count}')
