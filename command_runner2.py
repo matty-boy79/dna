d800f14681224ff9063dbaed5a1d5ce273d90031
Matt Watkins <mattyboy.watkins@gmail.com>
2020-12-04 14:05:22 +0000

    command runner now working

--- command_runner2.py
+++ command_runner2.py
@@ -1,8 +1,10 @@
 import time
 import json
 import urllib3
+import requests
 import variables
 from dnacentersdk import api
+from dnac_auth import get_auth_token
 
 # Set Constant variables
 SERVER = variables.DNAC_SERVER
@@ -17,15 +19,15 @@ urllib3.disable_warnings()
 dnac = api.DNACenterAPI(username=USER, password=PASSWORD, base_url=SERVER, version=VERSION, verify=False)
 
 # Create a list of commands to run, starting with "show run | inc hostname" <-- MANDATORY
-show_cmd = ["show run | inc hostname"] # DO NOT CHANGE THIS LINE
+show_cmd = ["show run | inc hostname"]  # DO NOT CHANGE THIS LINE
 
 # Add custom command to run here:
-show_cmd.append("show ip int b | inc Vlan")
+show_cmd.extend(["show ip int b | inc Vlan", "show run int lo0"])
 
 # Create a list of all devices to run the commands(s) on, by UUID
 devices = [
     "0da7fb1e-b9c4-41ac-af1e-5628dc00dee5",
-    #"66be433c-1c08-4f30-a023-82a84d460520"
+    "66be433c-1c08-4f30-a023-82a84d460520"
     # Add more device UUIDs above this line in quotes with a comma at the end
     # TODO: Take the output from device.get_device_list() and use this as the list of UUIDs
 ]
@@ -49,26 +51,56 @@ while task_status == "CLI Runner request creation":
 
 my_file_id = json.loads(task_status)['fileId']
 
+#######################################################################################################################
+
+token = get_auth_token()
+
+hdr = {
+        'content-type': 'application/json',
+        'X-Auth-Token': token
+    }
+
+SERVER = variables.DNAC_SERVER
+path = f'/dna/intent/api/v1/file/{my_file_id}'
+url = SERVER + path
+response = requests.get(url=url, headers=hdr, verify=False).text
+json = json.loads(response)
+
+for result in json:
+    for command in show_cmd:
+        print(result['commandResponses']['SUCCESS'][command])
+        print(command)
+    print("=" * 100)
+
+
+
 # Write a new header to the output file (overwrite previous file contents)
 # with open('command_output.txt', 'w') as file:
 #     file.write("Command Output\n")
 #     file.write("--------------\n")
 
-# Download the file from DNAC and convert it from a byte stream to a string
-file_str = dnac.file.download_a_file_by_fileid(file_id=my_file_id).data.decode()
-#print(json.dumps(file_str, indent=4))
-#exit()
+# # Download the file from DNAC and convert it from a byte stream to a string
+# file_str = dnac.file.download_a_file_by_fileid(file_id=my_file_id)
+# file_str_data = file_str.data
+# file_str_decoded = file_str_data.decode()
+# print(file_str_decoded)
+# #file_str = dnac.file.download_a_file_by_fileid(file_id=my_file_id).data.decode()
+# #print(json.dumps(file_str, indent=4))
+# #exit()
 
-# Convert the string to a JSON object so we can easily extract the parts we need
-file_json = json.loads(file_str)
-#print(file_json)
 
-print("=" * 100)
-print(file_json[0]['commandResponses']['SUCCESS'][show_cmd[0]])
-print("=" * 100)
-print(file_json[0]['commandResponses']['SUCCESS'][show_cmd[1]])
-print("=" * 100)
-
-# with open('command_output.txt', 'a') as file:
-#     file.write(file_contents)
+#
+#
+# # Convert the string to a JSON object so we can easily extract the parts we need
+# file_json = json.loads(file_str)
+# #print(file_json)
+#
+# print("=" * 100)
+# print(file_json[0]['commandResponses']['SUCCESS'][show_cmd[0]])
+# print("=" * 100)
+# print(file_json[0]['commandResponses']['SUCCESS'][show_cmd[1]])
+# print("=" * 100)
+#
+# # with open('command_output.txt', 'a') as file:
+# #     file.write(file_contents)
 #     file.write("\n\n################################################################################\n\n")
